2cf317f07e0a5421cc22e84793e69e2192198da6
Genn <intval@i.ua>
2021-07-19 09:50:27 +0300

    start

--- js/main.js
+++ js/main.js
@@ -215,7 +215,8 @@ shch.Slider = function (selectorSlide, activeForward, activeBack, setSons) {
         if (this.length > one) {
             this.Item[0].innerHTML = setSons[one]['contents'];
             this.Item[0].setAttribute('data-json-id', this.one);
-        } else {
+        }
+        if (this.length == one) {
             this.Item[0].innerHTML = setSons[0]['contents'];
             this.Item[0].setAttribute('data-json-id', 0);
             this.one = 0;
@@ -224,7 +225,8 @@ shch.Slider = function (selectorSlide, activeForward, activeBack, setSons) {
         if (this.length > two) {
             this.Item[1].innerHTML = setSons[two]['contents'];
             this.Item[1].setAttribute('data-json-id', this.two);
-        } else {
+        }
+        if (this.length == two) {
             this.Item[1].innerHTML = setSons[0]['contents'];
             this.Item[1].setAttribute('data-json-id', 0);
             this.two = 0;
@@ -233,7 +235,8 @@ shch.Slider = function (selectorSlide, activeForward, activeBack, setSons) {
         if (this.length > three) {
             this.Item[2].innerHTML = setSons[three]['contents'];
             this.Item[2].setAttribute('data-json-id', this.three);
-        } else {
+        }
+        if (this.length == three) {
             this.Item[2].innerHTML = setSons[0]['contents'];
             this.Item[2].setAttribute('data-json-id', 0);
             this.three = 0;
@@ -242,7 +245,8 @@ shch.Slider = function (selectorSlide, activeForward, activeBack, setSons) {
         if (this.length > four) {
             this.Item[3].innerHTML = setSons[four]['contents'];
             this.Item[3].setAttribute('data-json-id', this.four);
-        } else {
+        }
+        if (this.length == four) {
             this.Item[3].innerHTML = setSons[0]['contents'];
             this.Item[3].setAttribute('data-json-id', 0);
             this.three = 0;
@@ -251,7 +255,8 @@ shch.Slider = function (selectorSlide, activeForward, activeBack, setSons) {
         if (this.length > five) {
             this.Item[4].innerHTML = setSons[five]['contents'];
             this.Item[4].setAttribute('data-json-id', this.five);
-        } else {
+        }
+        if (this.length == five) {
             this.Item[4].innerHTML = setSons[0]['contents'];
             this.Item[4].setAttribute('data-json-id', 0);
             this.three = 0;
